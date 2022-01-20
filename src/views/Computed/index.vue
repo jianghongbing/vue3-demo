51e053e1bdfa06c05c9576b68523824201ca0bcb
jianghongbing <jianghongbing1989@qq.com>
2022-01-20 09:22:02 +0800

    update computed

--- src/views/Computed/index.vue
+++ src/views/Computed/index.vue
@@ -6,6 +6,10 @@
     <br />
     <button @click="obj.age++">increase age</button>
     <span>{{ age }}</span>
+    <br />
+    <button @click="agePlus++">increase age</button>
+    <span>{{ agePlus }}</span>
+    <button @click="agePlus--">decrease age</button>
   </div>
 </template>
 
@@ -18,12 +22,25 @@ export default {
       name: 'jiangxiaoyu',
       age: 10,
     })
+
+    // computed函数接收一个getter函数, 并根据getter的返回值返回一个不可变的响应式ref对象
     const age = computed(() => obj.age + 1)
     const upperCaseName = computed(() => obj.name.toUpperCase())
+
+    // 同时设置computed的setter && getter
+    const agePlus = computed({
+      set(value) {
+        obj.age = value - 1
+      },
+      get() {
+        return obj.age + 1
+      },
+    })
     return {
       obj,
       age,
       upperCaseName,
+      agePlus,
     }
   },
 }
--- src/views/Watch/index.vue
+++ src/views/Watch/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="watch">watch</div>
+  <div class="watch">
+    <button @click="count++">increase</button>
+    <span>{{ count }}</span>
+    <button @click="count--">decrease</button>
+  </div>
 </template>
 
 <script>
+import { ref, watchEffect } from 'vue'
 export default {
   name: 'v3-watch',
+  setup() {
+    const count = ref(0)
+    watchEffect(() => {
+      console.log(count)
+    })
+    setTimeout(() => {
+      count.value++
+    }, 1000)
+    return {
+      count,
+    }
+  },
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+button {
+  padding: 5px;
+}
+</style>
